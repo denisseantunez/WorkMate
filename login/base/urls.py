2b23571298a23d594ecfb6eace9e4ff6da58492a
Jorge Luna <jorday.02@gmail.com>
2024-10-18 21:21:15 -0700

    Adds correct logout functionality alongside blank dashboard features. Logout button suffers unfortunately :/

--- login/base/urls.py
+++ login/base/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path, include
 from .views import home
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("", home, name="home"),
     path("accounts/", include("django.contrib.auth.urls")),
+    path("login/", auth_views.LoginView.as_view(), name='login'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
 ]
