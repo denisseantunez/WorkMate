50f99224f85750f883d233df54530ab7eac74dfb
Jorge Luna <jorday.02@gmail.com>
2024-10-24 00:03:10 -0700

    HTMLs contiene funcionalidad para poder cerrar sesion correctamente. Se ve un poco dudoso en cuestion estetica pero es lo de menos.

--- login/base/urls.py
+++ login/base/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("", views.home, name="home"),
@@ -8,5 +9,7 @@ urlpatterns = [
     path("crear-nueva-tarea/", views.crear_nueva_tarea, name="crear-nueva-tarea"),
     path("mis-tareas/", views.mis_tareas, name="mis-tareas"),
     path("accounts/", include("django.contrib.auth.urls")),
+    path("login/", auth_views.LoginView.as_view(), name='login'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout')
 ]
 
