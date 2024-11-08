c447a55fe15cb78803cb5eaa9006f23356bd2e9f
Jorge Luna <jorday.02@gmail.com>
2024-11-08 08:32:36 -0700

    last last minute change to edit user

--- login/base/urls.py
+++ login/base/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     path("login/", auth_views.LoginView.as_view(), name='login'),
     path('accounts/logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
     path('editar-usuario/<int:user_id>/', views.editar_usuario, name='editar-usuario'),
-    path("crear-usuario/", views.gestion_crearusuario, name="crear-usuario"),  # Ensure this is correct
+    path("crear-usuario/", views.gestion_crearusuario, name="crear-usuario"),
 
 ]
 
